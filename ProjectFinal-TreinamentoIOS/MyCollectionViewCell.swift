09082b27ca2611835bd650785b280b88ca0a13bb
Pedro Moura <pedro.moura@socialbank.com.br>
2019-10-28 15:21:24 -0300

    First Commit

---
+++ ProjectFinal-TreinamentoIOS/MyCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  MyCollectionViewCell.swift
+//  ProjectFinal-TreinamentoIOS
+//
+//  Created by Pedro Henrique de Moura on 28/10/19.
+//  Copyright © 2019 Pedro Moura. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
+}
